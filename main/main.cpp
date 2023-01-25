5e77870aae6a0536c541bd44efd42f4b2a3caa1d
rshark93 <rsharcovich@yahoo.com>
2023-01-25 03:13:20 +0300

    init matplotlib

---
+++ main/main.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <vector>
+#include <matplot/matplot.h>
+
+using namespace matplot;
+
+int main() {
+    std::vector<double> x = linspace(0, 2 * pi);
+    std::vector<double> y = transform(x, [](auto x) { return sin(x); });
+
+    plot(x, y, "-o");
+    hold(on);
+    plot(x, transform(y, [](auto y) { return -y; }), "--xr");
+    plot(x, transform(x, [](auto x) { return x / pi - 1.; }), "-:gs");
+    plot({ 1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1 }, "k");
+
+    show();
+    return 0;
+}
