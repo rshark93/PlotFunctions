4a2b5068d93c3931a17e3676dd10f986f26a1a7c
rshark93 <rsharcovich@yahoo.com>
2023-01-25 05:31:58 +0300

    implementation

--- main/main.cpp
+++ main/main.cpp
@@ -1,19 +1,129 @@
+#include "main.h"
+
 #include <cmath>
-#include <vector>
-#include <matplot/matplot.h>
+#include <iomanip>
 
 using namespace matplot;
 
-int main() {
-    std::vector<double> x = linspace(0, 2 * pi);
-    std::vector<double> y = transform(x, [](auto x) { return sin(x); });
+calculate_function::calculate_function() {
+	std::cout << "calculate function constructor" << std::endl;
+}
 
-    plot(x, y, "-o");
-    hold(on);
-    plot(x, transform(y, [](auto y) { return -y; }), "--xr");
-    plot(x, transform(x, [](auto x) { return x / pi - 1.; }), "-:gs");
-    plot({ 1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1 }, "k");
+calculate_function::~calculate_function() {
+	std::cout << "\ncalculate_function destructor" << std::endl;
+}
 
-    show();
-    return 0;
+void calculate_function::init_plot_setting(const double x_start, const double x_end, const double y_min,
+	const double y_max, const int max_points)
+{
+	x_start_value_ = x_start;
+	x_end_value_ = x_end;
+
+	y_min_value_ = y_min;
+	y_max_value_ = y_max;
+
+	graph_max_points_ = max_points;
+}
+
+void calculate_function::calculate(const std::string& graph_name, double(* math_func)(double))
+{
+	const auto step = (abs(x_start_value_) + abs(x_end_value_)) / graph_max_points_;
+
+	std::vector<point2d> function_result;
+
+	for (auto x = x_start_value_; x < x_end_value_; x += step) {
+		auto y = math_func(x);
+
+		if(y < y_min_value_) y = y_min_value_;
+		if (y > y_max_value_) y = y_max_value_;
+
+		function_result.push_back(point2d{ x, y });
+	}
+
+	result_points_.insert(std::pair(graph_name, function_result ));
+}
+
+void calculate_function::print_result(const std::string& graph_name)
+{
+	if (!result_points_.count(graph_name)) {
+		std::cout << "Graph name Key not valid!";
+		return;
+	}
+
+	std::cout << "\n" << std::setw(20) << "PLOT: " << graph_name << std::endl;
+
+	std::cout << std::setw(5) << " " << "  ";
+	std::cout << std::setw(10) << "X:";
+	std::cout << std::setw(20) << "Y:" << std::endl;
+
+	const auto pts = result_points_[graph_name];
+
+	for (auto i = 0; i < pts.size() - 1; ++i) {
+		std::cout << std::setw(5) << i + 1 << ". ";
+		std::cout << std::setw(10) << pts[i].x;
+		std::cout << std::setw(20) << pts[i].y << std::endl;
+	}
+}
+
+int calculate_function::plot_graphic(const std::string& graph_name, const std::array<float, 3>& graph_color,
+                                     const float dot_size) {
+	std::vector<double> x_values;
+	std::vector<double> y_values;
+
+	for (const auto & [x, y] : result_points_[graph_name]) {
+		x_values.push_back(x);
+		y_values.push_back(y);
+	}
+
+	plot(x_values, y_values, "--gs")->line_width(2)
+	                                .marker_size(dot_size)
+	                                .color(graph_color)
+	                                .marker_color(GRAY)
+	                                .marker_face_color(BLACK);
+
+	hold(on);
+	show();
+	return 0;
+}
+
+// 10^tg(x)
+double math_fun_0(const double x) {
+	return pow(10, tan(x));
+}
+
+// 10^sin(x)
+double math_fun_1(const double x) {
+	return pow(10, sin(x));
+}
+
+int main(const int argc, char* argv[]) {
+	constexpr auto total_points = 21;
+	constexpr auto x_start = 0.;
+	constexpr auto x_end = 6.;
+
+	constexpr auto min_y = -50.f;
+	constexpr auto max_y = 50.f;
+
+	constexpr auto first_fun_name = "10^tg(x)";
+	constexpr auto second_fun_name = "10^sin(x)";
+
+	const auto calculate_obj = std::make_unique<calculate_function>();
+
+	calculate_obj->init_plot_setting(x_start, x_end, min_y, max_y, total_points);
+
+	calculate_obj->calculate(first_fun_name, math_fun_0);
+	calculate_obj->calculate(second_fun_name, math_fun_1);
+
+	calculate_obj->print_result(first_fun_name);
+	calculate_obj->print_result(second_fun_name);
+
+	constexpr auto dot_size = 5.f;
+
+	if(!calculate_obj->plot_graphic(second_fun_name, RED, dot_size))
+		return EXIT_FAILURE;
+	
+	if (!calculate_obj->plot_graphic(first_fun_name, GREEN, dot_size))
+		return EXIT_FAILURE;
+
+	return EXIT_SUCCESS;
 }
--- main/main.h
+++ main/main.h
@@ -1,1 +1,41 @@
+#pragma once
+
+#include <iomanip>
 #include <iostream>
+#include <map>
+#include <vector>
+
+#include <matplot/matplot.h>
+
+#define RED { 1., .0, .0 }
+#define GREEN { 0., 1., .0 }
+#define BLUE { 0., .0, 1. }
+#define BLACK { 0., 0., 0. }
+#define GRAY { .5, .5, .5 }
+#define WHITE { 1., 1., 1. }
+
+struct point2d {
+	double x;
+	double y;
+};
+
+class calculate_function {
+
+public:
+	void init_plot_setting(double x_start, double x_end, double y_min, double y_max, int max_points);
+	void calculate(const std::string& graph_name, double(*math_func)(double));
+	void print_result(const std::string& graph_name);
+	int plot_graphic(const std::string& graph_name, const std::array<float,3> &graph_color, float dot_size);
+
+	calculate_function();
+	~calculate_function();
+private:
+	double x_start_value_ = 0.;
+	double x_end_value_ = 10.;
+	double y_min_value_ = -1000.;
+	double y_max_value_ = 1000.;
+
+	int graph_max_points_ = 21;
+
+	std::map<std::string, std::vector<point2d>> result_points_;
+};
